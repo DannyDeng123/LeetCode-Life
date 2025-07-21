be2e096538c6ef5b74788360183f0a7c1816794c
Danny Deng <zxcdanny123@gmail.com>
2025-07-21 21:37:35 +0800

    No0009PalindromeNumber completed

---
+++ src/com/leetcode/life/No0009PalindromeNumber.java
@@ -0,0 +1,17 @@
+package com.leetcode.life;
+
+public class No0009PalindromeNumber {
+	public static void main(String[] args) {
+		System.out.println(isPalindrome(10));
+	}
+	public static boolean isPalindrome(int x) {
+		if(x<0 || (x%10==0 && x!=0))	return false;
+		
+		int y = 0;
+		while(x > y) {
+			y = (y * 10) + (x % 10);
+			x/=10;
+		}
+		return x==y || x==y/10;
+	}
+}
